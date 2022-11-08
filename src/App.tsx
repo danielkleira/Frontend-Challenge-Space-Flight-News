046f0d0592d1c90b5196242e2c45c3fde864fce1
danielkleira <daniel_leira@hotmail.com>
2022-11-07 22:48:28 -0300

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{  width: "100vw", height: "100vh"}}
+    ><div className=".content">
+      <p>
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
